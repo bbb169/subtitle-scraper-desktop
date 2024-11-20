3536b080e067a2a947f112ced9661a6705377ab9
jingzouzou <827088092@qq.com>
2024-11-21 00:52:22 +0800

    feat： try to read verify number

--- src/renderer/components/subtitleSiteRender.tsx
+++ src/renderer/components/subtitleSiteRender.tsx
@@ -33,17 +33,17 @@ export default forwardRef(function(props: IframeHTMLAttributes<any>, ref: RefObj
           //     .then(result => console.log(result));
           // });
           // 调用 Tesseract.js 进行识别
-          // recognize(imageBuffer, 'chi_sim', {
-          //   langPath: 'src/assets/',
-          //   gzip: false,
-          //   dataPath: 'chi_sim.traineddata'
-          // })
-          //   .then(({ data: { text } }) => {
-          //     console.log(`验证码 : ${text.trim()}`);
-          //   })
-          //   .catch((error) => {
-          //     console.error(`验证码  识别失败:`, error);
-          //   });
+          recognize(imageBuffer, 'chi_sim', {
+            langPath: 'src/assets/',
+            gzip: false,
+            dataPath: 'chi_sim.traineddata'
+          })
+            .then(({ data: { text } }) => {
+              console.log(`验证码 : ${text.trim()}`);
+            })
+            .catch((error) => {
+              console.error(`验证码  识别失败:`, error);
+            });
         } else {
           console.error('Element with class "verifyimg" not found.');
         }
@@ -52,35 +52,6 @@ export default forwardRef(function(props: IframeHTMLAttributes<any>, ref: RefObj
     subtitleSiteDom.addEventListener('did-fail-load', (event) => {
       console.error('Failed to load:', event);
     });
-      // subtitleSiteDom.contentWindow.document
-      // console.log('213',subtitleSiteDom.contentWindow.document.getElementsByClassName('verifyimg'));
-    // document.querySelector('#root > div > iframe').getElementsByClassName('verifyimg')('img')
-    // const $ = Cheerio.load(all.data);
-    // console.log('$: ');
-
-    // // 定义一个结果数组来存储解析的字幕信息
-    // const subtitles: any[] = [];
-
-    // // 假设字幕信息在某些特定的 HTML 元素中
-    // $('.verifyimg').each((index, element) => {
-    //   const imgSrc = $(element).attr('src'); // 获取图片路径
-    //   console.log('imgSrc: ', imgSrc);
-    //   try {
-    //     const imageBuffer = Buffer.from(imgSrc, 'base64');
-
-    //       // 调用 Tesseract.js 进行识别
-    //       recognize(imageBuffer, '')
-    //         .then(({ data: { text } }) => {
-    //           console.log(`验证码 ${index}: ${text.trim()}`);
-    //         })
-    //         .catch((error) => {
-    //           console.error(`验证码 ${index} 识别失败:`, error);
-    //         });
-    //     return !!subtitles
-    //   } catch (error) {
-    //     console.error(`获取验证码图片失败 (${imgSrc}):`, error);
-    //   }
-    // });
   }, [src])
 
   return <webview webpreferences="contextIsolation=no, nodeIntegration=yes" frameBorder="0" 
