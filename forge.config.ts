3b0d42d438f08ce5c0ecbed90f6f565fdd18bc5b
jingzouzou <827088092@qq.com>
2024-11-22 01:02:59 +0800

    feat: compelete compile static resource to build folder

--- forge.config.ts
+++ forge.config.ts
@@ -1,3 +1,4 @@
+
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
@@ -6,10 +7,13 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource:['./src/assets'],
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,4 +1,26 @@
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(() => {
+  let outputDir = '.vite/build'; // 默认输出目录
+
+  return {
+    plugins: [
+      copy({
+        targets: [
+          {
+            src: 'src/assets/chi_sim.traineddata', // 源文件路径
+            dest: `${outputDir}/assets`, // 输出目录
+          },
+        ],
+        hook: 'closeBundle', // 确保在打包结束后执行
+      }),
+      {
+        name: 'resolve-output-dir',
+        configResolved(resolvedConfig) {
+          outputDir = resolvedConfig.build.outDir; // 动态获取打包目录
+        },
+      },
+    ],
+  }
+});
