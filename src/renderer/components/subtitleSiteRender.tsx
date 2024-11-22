914cabcec4327457283f9af63a1736f973ee0e8c
jingzouzou <827088092@qq.com>
2024-11-23 00:31:19 +0800

    chore: format subtitle render codes

--- src/renderer/components/subtitleSiteRender.tsx
+++ src/renderer/components/subtitleSiteRender.tsx
@@ -1,54 +1,75 @@
 import { Button } from "antd";
-import { forwardRef, IframeHTMLAttributes, RefObject, useEffect, useRef } from "react";
+import {
+  forwardRef,
+  IframeHTMLAttributes,
+  RefObject,
+  useEffect,
+  useRef,
+} from "react";
 import { recognize } from "tesseract.js";
 
-
-export default forwardRef(function(props: IframeHTMLAttributes<any>, ref: RefObject<HTMLIFrameElement>) {
+export default forwardRef(function (
+  props: IframeHTMLAttributes<any>,
+  ref: RefObject<HTMLIFrameElement>
+) {
   const { src } = props;
   const subtitleSiteRef = useRef<HTMLWebViewElement>();
-  
+
   const getContent = () => {
     const subtitleSiteDom = subtitleSiteRef.current;
 
-    (subtitleSiteDom as any).executeJavaScript(`
+    (subtitleSiteDom as any)
+      .executeJavaScript(
+        `
       (function() {
         const element = document.querySelector('.verifyimg');
         return element ? element.getAttribute('src') : null;
       })();
-    `).then((html: any) => {
-      if (html) {
-        console.log('Element HTML:', html);
-        const imageBuffer = Buffer.from(html.split(',')[1], "base64");
+    `
+      )
+      .then((html: any) => {
+        if (html) {
+          const imageBuffer = Buffer.from(html.split(",")[1], "base64");
 
-        // 调用 Tesseract.js 进行识别
-        recognize(imageBuffer, 'eng')
-          .then(({ data: { text } }) => {
-            console.log(`验证码 : ${text.trim()}`);
-          })
-          .catch((error) => {
-            console.error(`验证码  识别失败:`, error);
-          });
-      } else {
-        console.error('Element with class "verifyimg" not found.');
-      }
-    });
-  }
+          // 调用 Tesseract.js 进行识别
+          recognize(imageBuffer, "eng")
+            .then(({ data: { text } }) => {
+              console.log(`验证码 : ${text.trim()}`);
+            })
+            .catch((error) => {
+              console.error(`验证码  识别失败:`, error);
+            });
+        } else {
+          console.error('Element with class "verifyimg" not found.');
+        }
+      });
+  };
 
   useEffect(() => {
     const subtitleSiteDom = subtitleSiteRef.current;
-    subtitleSiteDom.addEventListener('dom-ready', () => {
+    subtitleSiteDom.addEventListener("dom-ready", () => {
       getContent();
-     
     });
-    subtitleSiteDom.addEventListener('did-fail-load', (event) => {
-      console.error('Failed to load:', event);
+    subtitleSiteDom.addEventListener("did-fail-load", (event) => {
+      console.error("Failed to load:", event);
     });
-  }, [src])
+  }, [src]);
 
-  return <>
-    <Button onClick={() => { 
-      getContent();
-     }}>click to get content</Button>
-    <webview webpreferences="contextIsolation=no, nodeIntegration=yes" frameBorder="0" {...props} ref={subtitleSiteRef}></webview>
-  </>
-})
+  return (
+    <>
+      <Button
+        onClick={() => {
+          getContent();
+        }}
+      >
+        click to get content
+      </Button>
+      <webview
+        webpreferences="contextIsolation=no, nodeIntegration=yes"
+        frameBorder="0"
+        {...props}
+        ref={subtitleSiteRef}
+      ></webview>
+    </>
+  );
+});
