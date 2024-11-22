c5e2d82ea5c0b47d267240c47d6f62bd61c11d00
jingzouzou <827088092@qq.com>
2024-11-23 00:30:42 +0800

    chore: delete unused comments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import axios from 'axios';
-import { app, BrowserWindow, ipcMain, session } from 'electron';
+import { app, BrowserWindow, session } from 'electron';
 import path from 'path';
-import * as cheerio from 'cheerio';
-import { recognize } from 'tesseract.js';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -14,75 +11,6 @@ const createWindow = async () => {
     // 忽略所有证书验证错误
     callback(0);
   });
-  try {
-    // // 发起 HTTP 请求
-    // const all = await axios.request({
-    //   maxRedirects: 5,
-    //   url: 'http://so.zimuku.org/search',
-    //   method: 'GET',
-    //   params: {
-    //     q: '金刚狼',
-    //     chost: 'zimuku.org',
-    //   },
-    //   validateStatus: function () {
-    //     // 返回 true 表示所有状态码都被视为有效
-    //     return true;
-    //   },
-    // });
-    // // console.log('data: ', all.status, all.data);
-
-    // // recognize()
-    // // 使用 Cheerio 加载 data
-    // const $ = cheerio.load(all.data);
-    // console.log('$: ');
-
-    // // 定义一个结果数组来存储解析的字幕信息
-    // const subtitles: any[] = [];
-
-    // // 假设字幕信息在某些特定的 HTML 元素中
-    // $('.verifyimg').each((index, element) => {
-    //   const imgSrc = $(element).attr('src'); // 获取图片路径
-    //   console.log('imgSrc: ', imgSrc);
-    //   try {
-    //     const imageBuffer = Buffer.from(imgSrc, 'base64');
-
-    //       // 调用 Tesseract.js 进行识别
-    //       recognize(imageBuffer, '')
-    //         .then(({ data: { text } }) => {
-    //           console.log(`验证码 ${index}: ${text.trim()}`);
-    //         })
-    //         .catch((error) => {
-    //           console.error(`验证码 ${index} 识别失败:`, error);
-    //         });
-    //     return !!subtitles
-    //   } catch (error) {
-    //     console.error(`获取验证码图片失败 (${imgSrc}):`, error);
-    //   }
-    // });
-
-    // return subtitles;
-    ipcMain.handle("recognizeVerifyNumber", async (_, imgBase64: string) => {
-      const imageBuffer = Buffer.from(imgBase64, "base64");
-      const rootPath = path.resolve(__dirname, "./assets");
-      console.log('rootPath: ', rootPath);
-
-      try {
-        const { data: { text } } = await recognize(imageBuffer, "chi_sim", {
-          langPath: rootPath,
-          gzip: false,
-          dataPath: "chi_sim.traineddata",
-        })
-        console.log(`验证码 : ${text.trim()}`);
-        return text.trim()
-      } catch (error) {
-        console.error(`验证码  识别失败:`, error);
-        return null
-      }
-    });
-  } catch (error) {
-    console.log('error: ', error);
-    
-  }
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
