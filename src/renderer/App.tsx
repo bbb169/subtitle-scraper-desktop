ad48a1c0a8ab70b4dc2f2375f7f22cea80c4b248
jingzouzou <827088092@qq.com>
2024-11-24 18:45:14 +0800

    feat: add mutiplie ways to search subtitle

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,15 +1,39 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { stringify } from 'qs';
 import SubtitleSiteRender from './components/subtitleSiteRender'
+import { Button, Input, Space } from 'antd';
+import { debounce }  from 'lodash';
+import FileDragger from './components/fileDragger';
+import { useRequest } from 'ahooks';
+const { Search } = Input;
 
 export default function () {
+  const [searchValue, setSearchValue] = useState<string>('');
+  console.log('searchValue: ', searchValue);
 
-  return <div>
-    <div>2132</div>
-    <SubtitleSiteRender src={`https://so.zimuku.org/search?${stringify({
-      q: '金刚狼',
+  const { data: searchSubtitleValue, run: searchSubtitle, mutate: searchSubtitleSync } = useRequest(async (value?: string) => {
+    return value || searchValue;
+  }, {
+    debounceWait: 1000,
+    refreshDeps: [searchValue]
+  })
+
+
+  return <Space direction='vertical' style={{ width: '100%' }}>
+    <Search placeholder="输入您要搜索的字幕" value={searchValue} onChange={(evt) => {
+      setSearchValue(evt.target.value);
+    }} onSearch={(value) => {
+      searchSubtitleSync(value)
+    }} enterButton />
+    <FileDragger onFilenameHandled={(value) => {
+      setSearchValue(value);
+      searchSubtitleSync(value);
+    }} />
+    {searchSubtitleValue}
+    {searchValue && <SubtitleSiteRender src={`https://so.zimuku.org/search?${stringify({
+      q: searchValue,
       chost: 'zimuku.org',
-    })}`}/>
-  </div>
+    })}`}/>}
+  </Space>
 }
