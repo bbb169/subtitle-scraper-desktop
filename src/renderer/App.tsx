485015a814da19f1dcc554abc9fd91f6b1b8900a
jingzouzou <827088092@qq.com>
2024-11-27 22:39:29 +0800

    chore: delete unused modules

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from 'react'
 import { stringify } from 'qs';
 import SubtitleSiteRender from './components/subtitleSiteRender'
 import { Button, Input, Space } from 'antd';
-import { debounce }  from 'lodash';
 import FileDragger from './components/fileDragger';
 import { useRequest } from 'ahooks';
 const { Search } = Input;
@@ -12,7 +10,7 @@ export default function () {
   const [searchValue, setSearchValue] = useState<string>('');
   console.log('searchValue: ', searchValue);
 
-  const { data: searchSubtitleValue, run: searchSubtitle, mutate: searchSubtitleSync } = useRequest(async (value?: string) => {
+  const { data: searchSubtitleValue, mutate: searchSubtitleSync } = useRequest(async (value?: string) => {
     return value || searchValue;
   }, {
     debounceWait: 1000,
