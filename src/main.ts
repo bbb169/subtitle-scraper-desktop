0a27e3018f3fc356059e1b4b2a3288dbfbfa8028
jingzouzou <827088092@qq.com>
2024-11-29 23:16:15 +0800

    chore: format codes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { app, BrowserWindow, dialog, ipcMain, ipcRenderer, session, webUtils } from 'electron';
-import path from 'path';
+import {
+  app,
+  BrowserWindow,
+  dialog,
+  ipcMain,
+  ipcRenderer,
+  session,
+  webUtils,
+} from "electron";
+import * as fs from "node:fs";
+import path from "path";
+import https from "https";
+import http from "http";
+import axios from "axios";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -17,7 +29,7 @@ const createWindow = async () => {
     width: 1400,
     height: 900,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true, // 如果需要 Node.js 支持
       contextIsolation: false, // 如果需要共享上下文
       webSecurity: false, // 禁用同源策略
@@ -30,20 +42,26 @@ const createWindow = async () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
   // 打开文件选择对话框
-  ipcMain.handle('openUploadDialog', async () => {
+  ipcMain.handle("openUploadDialog", async () => {
     const options = {
-      title: '选择文件',
-      buttonLabel: '确认',
+      title: "选择文件",
+      buttonLabel: "确认",
     };
 
-    return dialog.showOpenDialog(mainWindow, options)
+    return dialog.showOpenDialog(mainWindow, options);
+  });
+
+  ipcMain.handle("useFs", async () => {
+    return fs;
   });
 
   ipcMain.handle("downloadFile", async (_, fileUrl: string, saveDir: string) => {
@@ -114,18 +132,18 @@ const createWindow = async () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
