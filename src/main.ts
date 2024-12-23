f5af838bc6bc4f6783db294f751040291b477b01
jingzouzou <827088092@qq.com>
2024-12-23 22:49:58 +0800

    feat: hide menu bar

--- src/main.ts
+++ src/main.ts
@@ -107,6 +107,7 @@ const createWindow = async () => {
       allowRunningInsecureContent: true,
       webviewTag: true, // 启用 webview
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
@@ -231,7 +232,9 @@ const createWindow = async () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow()
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
