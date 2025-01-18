7c7295849d97ac01895afb3e7df9389d11f3a6ea
bbb169 <827088092@qq.com>
2025-01-18 21:12:34 +0800

    feat: add public function to get enum

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -1,3 +1,6 @@
+import { CheckboxOptionType } from "antd";
+import { ReactNode } from "react";
+
 export const tryCathWrapper = async (
   callback: () => Promise<any>,
   customPrefix = "tryCathWrapper error: "
@@ -8,3 +11,16 @@ export const tryCathWrapper = async (
     console.log(customPrefix, error);
   }
 };
+
+export const getEnumOptions = <K extends string | number, V extends ReactNode>(map: Map<K, V>)  => {
+  const options: CheckboxOptionType[] = [];
+
+  for (const [key, value] of map) {
+    options.push({
+      label: value,
+      value: key,
+    })
+  }
+
+  return options
+}
