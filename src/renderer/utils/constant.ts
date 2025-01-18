140aa45d2c4bf2df4c0d6fac32e88967b4734411
bbb169 <827088092@qq.com>
2025-01-18 21:12:52 +0800

    feat: add subtitle source map

---
+++ src/renderer/utils/constant.ts
@@ -0,0 +1,7 @@
+export const enum subtitleSourceEnum {
+  zimuku = 'zimuku',
+  tang = '98tang'
+}
+
+export const subtitleSourceMap = new Map([["字幕库",subtitleSourceEnum.zimuku ],
+  ["大堂",subtitleSourceEnum.tang ]])
